1d104d8bade4cdf9f080474746b86bf1fd2d86a6
JustinReisz <justinRE@users.noreply.github.com>
2022-05-01 14:59:53 -0400

    Added more stats

--- disk.cs
+++ disk.cs
@@ -1,13 +1,26 @@
 namespace cs360{
     public class Disk{
         private static int MOVEMENT_TIME_CONSTANT = 12;
+        private static double TRANSFER_TIME_BASE = 1.2;
         private static int ROTATION_DELAY = 5;
         private int rotationCounter = 0;
         private int totalInstructionsProcessed = 0;
 
+        private double totalSeekTime = 0;
+
         public int SearchTime
         {
             get { return rotationCounter * ROTATION_DELAY; }
+        }
+
+        public double TransferTime
+        {
+            get { return totalInstructionsProcessed * TRANSFER_TIME_BASE; }
+        }
+
+        public double AccessTime
+        {
+            get { return TransferTime+SearchTime+totalSeekTime; }
         }
 
         public double calculateSeekTime(Instruction previousInstruction,Instruction currentInstruction){
@@ -22,6 +35,8 @@ namespace cs360{
             }
             double seekTime = MOVEMENT_TIME_CONSTANT + (0.1 * seekTimeTmp);
 
+            totalSeekTime= totalSeekTime+seekTime;
+
             return seekTime;
 
         }
@@ -29,7 +44,13 @@ namespace cs360{
         public String getStats(){
             return "\n:::Disk Stats::: " + 
                    "\nDisk Search Time: " + this.SearchTime + 
-                   "\nTotal Instructions: " + this.totalInstructionsProcessed;
+                   "\nTotal Instructions: " + this.totalInstructionsProcessed +
+                   "\nTotal seek time: " + this.totalSeekTime +
+                   "\nTotal Transfer time: " + this.TransferTime +
+                   "\nAccess time: " + this.AccessTime;
+
+
+                   
         }
     }
 }
