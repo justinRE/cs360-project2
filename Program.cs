024fbe77051cbd588b14153e921ef02215edb27d
JustinReisz <justinRE@users.noreply.github.com>
2022-05-01 10:31:04 -0400

    new stuff

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, justin!");
+﻿using System;
+using static System.Console;
+
+WriteLine("Hello, justin!");
+
+int[,] data = new int[,] {{ 0, 50, 9},{23,135,6},{25,20,2},{29,25,1},{35,200,9},{45,175,5},{57,180,3},{83,99,4},{88,73,5},{95,199,9} };
