29cd191ad7f187b6f48e1883a76d263d9fdc99ae
JustinReisz <justinRE@users.noreply.github.com>
2022-05-01 11:47:35 -0400

    added the rest of the data

--- Program.cs
+++ Program.cs
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-﻿// Hello World! program
 namespace cs360
 {
     class Project2 {         
@@ -19,6 +17,14 @@ namespace cs360
             List<ReadDiskRequest> data = new List<ReadDiskRequest>();
             data.Add(new ReadDiskRequest(0, 50, 9));
             data.Add(new ReadDiskRequest(23, 135, 6));
+            data.Add(new ReadDiskRequest(25, 20, 2));
+            data.Add(new ReadDiskRequest(29, 25, 1));
+            data.Add(new ReadDiskRequest(35, 200, 9));
+            data.Add(new ReadDiskRequest(45, 175, 5));
+            data.Add(new ReadDiskRequest(57, 180, 3));
+            data.Add(new ReadDiskRequest(83, 99, 4));
+            data.Add(new ReadDiskRequest(88, 73, 5));
+            data.Add(new ReadDiskRequest(95, 199, 6));
             return data;
         }
     }
