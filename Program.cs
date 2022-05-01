b330f8959d1a6c2b9e67f9760e048e128745f3ac
Wesley Reisz <wes@wesleyreisz.com>
2022-05-01 13:01:17 -0400

    added seek time to program

--- Program.cs
+++ Program.cs
@@ -1,30 +1,32 @@
 namespace cs360
 {
     class Project2 {         
+        private int _headLocation = 0;
+
         static void Main(string[] args)
         {
             System.Console.WriteLine("Starting Assignment 2");
             
             Project2 project2 = new Project2();
-            List<ReadDiskRequest> data = project2.loadData();
+            IList<Instruction> instructions = project2.loadData();
 
-            foreach (ReadDiskRequest item in data){
+            foreach (Instruction item in instructions){
                  System.Console.WriteLine(item);
             }           
         }
 
-        private List<ReadDiskRequest> loadData(){
-            List<ReadDiskRequest> data = new List<ReadDiskRequest>();
-            data.Add(new ReadDiskRequest(0, 50, 9));
-            data.Add(new ReadDiskRequest(23, 135, 6));
-            data.Add(new ReadDiskRequest(25, 20, 2));
-            data.Add(new ReadDiskRequest(29, 25, 1));
-            data.Add(new ReadDiskRequest(35, 200, 9));
-            data.Add(new ReadDiskRequest(45, 175, 5));
-            data.Add(new ReadDiskRequest(57, 180, 3));
-            data.Add(new ReadDiskRequest(83, 99, 4));
-            data.Add(new ReadDiskRequest(88, 73, 5));
-            data.Add(new ReadDiskRequest(95, 199, 6));
+        private IList<Instruction> loadData(){
+            IList<Instruction> data = new List<Instruction>();
+            data.Add(new Instruction(0, 50, 9));
+            data.Add(new Instruction(23, 135, 6));
+            data.Add(new Instruction(25, 20, 2));
+            data.Add(new Instruction(29, 25, 1));
+            data.Add(new Instruction(35, 200, 9));
+            data.Add(new Instruction(45, 175, 5));
+            data.Add(new Instruction(57, 180, 3));
+            data.Add(new Instruction(83, 99, 4));
+            data.Add(new Instruction(88, 73, 5));
+            data.Add(new Instruction(95, 199, 6));
             return data;
         }
     }
--- ReadDiskRequest.cs
+++ ReadDiskRequest.cs
@@ -1,15 +1,45 @@
-public class ReadDiskRequest{
+public class Instruction{
+    private static int MOVEMENT_TIME_CONSTANT = 12;
     private int _arrivalTime = 0;
     private int _trackRequest = 0;
     private int _sectorRequest = 0;
+    private double _seekTime = 0;
 
-    public ReadDiskRequest (int arrivalTime, int trackRequest, int sectorRequest){
+    public int ArrivalTime
+    {
+        get { return _arrivalTime; }
+        set { _arrivalTime = value; }
+    }
+    public int TrackRequest
+    {
+        get { return _trackRequest; }
+        set { _trackRequest = value; }
+    }
+     public int SectorRequest
+    {
+        get { return _sectorRequest; }
+        set { _sectorRequest = value; }
+    }
+    public double SeekTime
+    {
+        get { return _seekTime; }
+    }
+
+    public double calculateSeekTime(int originalHeadLocation){
+        //calculate _seekTime
+        _seekTime = MOVEMENT_TIME_CONSTANT + (0.1 * Math.Abs(originalHeadLocation-_trackRequest));
+        return _seekTime;
+    }
+
+    public Instruction (int arrivalTime, int trackRequest, int sectorRequest){
         this._arrivalTime = arrivalTime;
         this._trackRequest = trackRequest;
         this._sectorRequest = sectorRequest;
     }
 
-     public override string ToString()
+   
+
+    public override string ToString()
     {
         return "ReadDiskRequest[_arrivalTime: " + _arrivalTime+ ",_trackRequest: " + _trackRequest+ ",_sectorRequest: " + _sectorRequest+"]";
     }
