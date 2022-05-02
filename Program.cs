d34342931307a6c1fa76bb20f47e94050dfa6c5f
Wesley Reisz <wes@wesleyreisz.com>
2022-05-01 21:35:11 -0400

    added operator to sort for sstf

--- Program.cs
+++ Program.cs
@@ -10,10 +10,43 @@ namespace cs360
             //this is where I need to handle other comparators for different sort order
             IList<Instruction> instructions = project2.loadData();
             project2.performFCFS(instructions);
+            project2.performSSTF(instructions);
 
            
             
         }
+
+         private void performSSTF( IList<Instruction> instructions){
+             List<Instruction> sortedListInstructions = instructions.OrderBy(i=>i.SectorRequest).ToList();
+             IComparer<Instruction> comparer = new TrackRequestComparer();
+             sortedListInstructions.Sort(comparer);
+             instructions = new List<Instruction>();
+             instructions = sortedListInstructions;
+
+             IList<Double> accessTimes = new List<Double>();
+
+             Disk disk = new Disk();
+
+             for (int i=0;i<instructions.Count; i++){
+                Instruction currentInstruction = instructions[i];
+                
+                //check for outofbounds
+                if (i <=0){
+                    disk.calculateSeekTime(new Instruction(), currentInstruction);
+                }else{
+                    disk.calculateSeekTime(instructions[i-1],currentInstruction);
+                }
+                //Console.Out.WriteLine(instructions[i]); 
+                accessTimes.Add(disk.AccessTime); 
+             }
+             Console.Out.WriteLine("SSTF Stats");
+             Console.Out.WriteLine(disk.getStats());
+
+             double average = disk.AverageAccessTime;
+             double sum = accessTimes.Sum(d => Math.Pow(d - average, 2));
+             Console.Out.WriteLine("Access Time Standard Deviation: " + Math.Sqrt((sum) / accessTimes.Count()));
+             Console.Out.WriteLine("Access Time Variance: " + sum / (accessTimes.Count()-1));
+         }
 
         private void performFCFS( IList<Instruction> instructions){
             IList<Double> accessTimes = new List<Double>();
---
+++ TrackRequestComparer.cs
@@ -0,0 +1,13 @@
+namespace cs360{
+    public class TrackRequestComparer : IComparer<Instruction>{
+        public int Compare(Instruction x, Instruction y)
+        {
+            int compareTrack = x.TrackRequest.CompareTo(y.TrackRequest);
+            if (compareTrack == 0)
+            {
+                return x.TrackRequest;
+            }
+            return compareTrack;
+        }
+    }
+}
